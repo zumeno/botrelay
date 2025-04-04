02622767bdff5c08ac5b659437824a017c74d691
arungeorgesaji <arungeorgesaji@gmail.com>
2025-04-04 09:27:27 +0530

    Added run also started fixing and trying current functionality

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ import subprocess
 def create_new_ai(username, ai_name):
     dir_path = f"ais/{username}/{ai_name}"
     
-    subprocess.run(["mkdir -p", dir_name])
-    subprocess.run(["cp -r template", dir_path])
+    subprocess.run(["mkdir", "-p", dir_path])
+    subprocess.run(["cp", "-r", "template", dir_path])
 
 def train_ai(username, ai_name):
     dir_path = f"ais/{username}/{ai_name}"
     
     subprocess.run(
         ["rasa", "train"],
+        cwd=dir_path,  
+        check=True
+    )
+
+def run_ai(username, ai_name):
+    dir_path = f"ais/{username}/{ai_name}"
+    
+    subprocess.run(
+        ["rasa", "shell"],
         cwd=dir_path,  
         check=True
     )
